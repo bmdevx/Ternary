5cb79244ab808dd0b0b954e72ab941716f088c81
Bryan Miller <bryancamrn@gmail.com>
2018-05-29 01:01:36 -0600

    Added overloads to Wire

--- Core/Components/Wire.cs
+++ Core/Components/Wire.cs
@@ -12,5 +12,54 @@ namespace Ternary.Components
         {
             Output?.Invoke(sender, trit);
         }
+
+
+        /// <summary>
+        /// Attaches a component event to listen to the wire
+        /// </summary>
+        /// <param name="wire">Wire that is being listened to</param>
+        /// <param name="input">Component event that will listen to wire</param>
+        /// <returns></returns>
+        public static Wire operator +(Wire wire, ComponentTriggeredEvent input)
+        {
+            wire.Output += input;
+            return wire;
+        }
+
+        /// <summary>
+        /// Attaches a wire to a component
+        /// </summary>
+        /// <param name="input">Component event that the wire listens to</param>
+        /// <param name="wire">Wire that will listen to a component event</param>
+        /// <returns></returns>
+        public static ComponentTriggeredEvent operator +(ComponentTriggeredEvent input, Wire wire)
+        {
+            input += wire.Input;
+            return input;
+        }
+
+        /// <summary>
+        /// Attaches a wire to a component
+        /// </summary>
+        /// <param name="component">Component that the wire listens to</param>
+        /// <param name="wire">Wire that will listen to a component</param>
+        /// <returns></returns>
+        public static IComponent operator +(IComponent component, Wire wire)
+        {
+            component.Output += wire.Input;
+            return component;
+        }
+
+        /// <summary>
+        /// Attaching one wire to another
+        /// </summary>
+        /// <param name="outputWire">Wire that will be listened to</param>
+        /// <param name="inputWire">Wire that will listen to the other wire</param>
+        /// <returns></returns>
+        public static Wire operator +(Wire outputWire, Wire inputWire)
+        {
+            outputWire.Output += inputWire.Input;
+            return outputWire;
+        }
     }
 }
