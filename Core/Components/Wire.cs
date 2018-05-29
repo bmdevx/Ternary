46bfdae5ea9f6a9333073a537b8179806428680f
Bryan Miller <bryancamrn@gmail.com>
2018-05-29 01:13:09 -0600

    Wire update

--- Core/Components/Wire.cs
+++ Core/Components/Wire.cs
@@ -14,8 +14,9 @@ namespace Ternary.Components
         }
 
 
+
         /// <summary>
-        /// Attaches a component event to listen to the wire
+        /// Attaches a component event to listen a wire
         /// </summary>
         /// <param name="wire">Wire that is being listened to</param>
         /// <param name="input">Component event that will listen to wire</param>
@@ -25,6 +26,19 @@ namespace Ternary.Components
             wire.Output += input;
             return wire;
         }
+
+        /// <summary>
+        /// Detaches a component event from listening to a wire
+        /// </summary>
+        /// <param name="wire">Wire that will no longer be listened to</param>
+        /// <param name="input">Component event that will no longer listen the wire</param>
+        /// <returns></returns>
+        public static Wire operator -(Wire wire, ComponentTriggeredEvent input)
+        {
+            wire.Output += input;
+            return wire;
+        }
+
 
         /// <summary>
         /// Attaches a wire to a component event
@@ -39,6 +53,19 @@ namespace Ternary.Components
         }
 
         /// <summary>
+        /// Detaches a wire to a component event
+        /// </summary>
+        /// <param name="output">Component event that the wire will no longer listen to</param>
+        /// <param name="wire">Wire that will no longer listen to the component event</param>
+        /// <returns></returns>
+        public static ComponentTriggeredEvent operator -(ComponentTriggeredEvent output, Wire wire)
+        {
+            output += wire.Input;
+            return output;
+        }
+
+
+        /// <summary>
         /// Attaches a wire to a component
         /// </summary>
         /// <param name="component">Component that the wire listens to</param>
@@ -51,12 +78,37 @@ namespace Ternary.Components
         }
 
         /// <summary>
+        /// Detaches a wire to a component
+        /// </summary>
+        /// <param name="component">Component that the wire will no longer listen to</param>
+        /// <param name="wire">Wire that will no longer listen to the component</param>
+        /// <returns></returns>
+        public static IComponent operator -(IComponent component, Wire wire)
+        {
+            component.Output += wire.Input;
+            return component;
+        }
+
+
+        /// <summary>
         /// Attaching one wire to another
         /// </summary>
         /// <param name="outputWire">Wire that will be listened to</param>
         /// <param name="inputWire">Wire that will listen to the other wire</param>
         /// <returns></returns>
         public static Wire operator +(Wire outputWire, Wire inputWire)
+        {
+            outputWire.Output += inputWire.Input;
+            return outputWire;
+        }
+
+        /// <summary>
+        /// Detaching one wire from another
+        /// </summary>
+        /// <param name="outputWire">Wire that will no longer be listened to</param>
+        /// <param name="inputWire">Wire that will no longer listen to the other wire</param>
+        /// <returns></returns>
+        public static Wire operator -(Wire outputWire, Wire inputWire)
         {
             outputWire.Output += inputWire.Input;
             return outputWire;
